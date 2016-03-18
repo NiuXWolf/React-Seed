7a24b6ebd8d0f9125cb202cf00b0f985621bc817
YuYang <god_err0r@yeah.net>
2016-03-18 10:43:38 +0800

    semantic

--- tools/copyfiles.js
+++ tools/copyfiles.js
@@ -1,5 +1,4 @@
 import path from 'path';
 import copyDir from 'copy-dir';
-// copyDir('static/fonts', 'dist/fonts');
-// copyDir('static/css', 'dist/css');
+copyDir('ui/semantic','dist');
 copyDir('static','dist');
